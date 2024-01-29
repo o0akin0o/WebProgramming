b3531d5e97fc195b1f655732bc6fee90d4a996e7
o0akin0o <lenhatminhquan@gmail.com>
2024-01-29 10:52:34 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Website Title</title>
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+            rel="stylesheet" />
+    <style>
+        nav{
+            background-color: #4E7047;
+            color: white;
+        }
+        ul {
+            list-style-type: none;
+            float: right;
+            padding-top: 10px;
+            padding-right: 20px;
+        }
+        li {
+            float: left;
+            padding: 15px;
+            font-size: 20px;
+        }
+    </style>
+</head>
+    <body>
+    <header>
+    <nav class="navbar row">
+                <h2 class="col-md-4 logo">Easter Kitchen</h2>
+                <div class="col-md-8">
+
+                    <ul>
+                        <li>
+                            <a class="nav-link" href="ex1.php">EX1</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="ex2.php">EX2</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">EX3</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">EX4</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+    </body>
+</header>
