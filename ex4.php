5cdd3ff74023087d1c89206aa963aa7da1a0b124
o0akin0o <lenhatminhquan@gmail.com>
2024-02-01 23:52:39 +0200

    Add files via upload

--- ex4.php
+++ ex4.php
@@ -7,8 +7,15 @@
 <body>
     <?php include 'header.php'; ?>
     <h1>If-Else</h1>
+    <form method="post" action="">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age" required>
+        <button type="submit" name="submitifelse">Check Eligibility</button>
+    </form>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (($_SERVER["REQUEST_METHOD"] == "POST")&& (isset($_POST["submitifelse"]))) {
         $name = $_POST["name"];
         $age = $_POST["age"];
 
@@ -20,13 +27,7 @@
     }
     ?>
 
-    <form method="post" action="">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        <label for="age">Age:</label>
-        <input type="number" id="age" name="age" required>
-        <button type="submit">Check Eligibility</button>
-    </form>
+    
     <h1>Switch Case</h1>
     <form>
         <?php
@@ -42,29 +43,37 @@
         ?>
     </form>
     <h1>For Loop</h1>
-    <form>
-        <?php
-        $n = 5; 
-        echo "<p>Multiplication table for $n:</p>";
-
-        for ($i = 1; $i <= 10; $i++) {
-            echo "$n x $i = " . ($n * $i) . "<br>";
+    <form method="post" action="">
+        <label for="name">Input n:</label>
+        <input type="number" id="n" name="n" required>
+        <button type="submit" name="submitfor">Submit n</button>
+    </form>
+    <?php
+        if (($_SERVER["REQUEST_METHOD"] == "POST")&& (isset($_POST["submitfor"]))) {
+            $n = $_POST["n"];
+            echo "<p>Multiplication table for $n:</p>";
+            for ($i = 1; $i <= 10; $i++) {
+                echo "$n x $i = " . ($n * $i) . "<br>";
+            }
         }
         ?>
-    </form>
     <h1>While Loop</h1>
-    <form>
-        <?php
-        $n = 5;
-        echo "<p>Numbers from 1 to $n:</p>";
-
-        $i = 1;
-        while ($i <= $n) {
-            echo "$i ";
-            $i++;
-        }
-        ?>
+    <form method="post" action="">
+        <label for="name">Input m:</label>
+        <input type="number" id="m" name="m" required>
+        <button type="submit" name="submitwhile">Submit m</button>
     </form>
+    <?php
+        if (($_SERVER["REQUEST_METHOD"] == "POST")&& (isset($_POST["submitwhile"]))) {
+            $m = $_POST["m"];
+            echo "<p>Numbers from 1 to $m:</p>";
+            $i = 1;
+            while ($i <= $m) {
+                echo "$i ";
+                $i++;
+            }
+        }
+    ?>
     <h1>Foreach Loop</h1>
     <form>
         <?php
